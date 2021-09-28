5d2b76aea0948ac01707f8db712dc9e78fcad8c6
martam90 <banas.m.marta@gmail.com>
2021-09-28 10:35:53 +0200

    add tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      cyan: {
+        dark: '#5ba4a4',
+        grayish: '#effafa', 
+        lightGrayish: '#eef6f6',
+        darkGrayish: '#7b8e8e',
+        veryDarkGrayish: '#2c3a3a',
+        white: '#ffffff',
+      },
+    },
+    fontFamily: {
+      'sans': ['Spartan'],
+    },
+    borderColor: (theme) => ({
+      ...theme('colors'),
+    }),
+    extend: {
+      backgroundImage: {
+        'header-mobile': "url('./images/bg-header-mobile.svg')",
+        'header-desktop': "url('./images/bg-header-desktop.svg')",
+      }
+    },
+    screens: {
+      'md': '995px',
+    }
   },
   variants: {
     extend: {},
