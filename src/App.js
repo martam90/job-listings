5fb56c7f03ff88352eea9ffddf475a61f0597c09
martam90 <banas.m.marta@gmail.com>
2021-09-28 10:38:23 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
